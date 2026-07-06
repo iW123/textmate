86af46b0d01f28c5586531686b5871236e06455a
iW123 <65850431+iW123@users.noreply.github.com>
2026-07-06 08:20:42 +0800

    Update OakDocumentView.mm

--- Frameworks/OakTextView/src/OakDocumentView.mm
+++ Frameworks/OakTextView/src/OakDocumentView.mm
@@ -367,7 +367,7 @@ static NSString* const kFoldingsColumnIdentifier  = @"foldings";
 		gutterView.selectionBorderColor      = [NSColor colorWithCGColor:styles.selectionBorder];
 		gutterScrollView.backgroundColor     = gutterView.backgroundColor;
 //        gutterScrollView.appearance          = textScrollView.effectiveAppearance;
-//        gutterScrollView.allowsVibrancy      = YES;
+        gutterScrollView.allowsVibrancy      = YES;
 		gutterDividerView.activeBackgroundColor = [NSColor colorWithCGColor:styles.divider];
 
 		[gutterView setNeedsDisplay:YES];
@@ -664,7 +664,7 @@ static NSString* const kFoldingsColumnIdentifier  = @"foldings";
 
 - (CGFloat)widthForColumnWithIdentifier:(id)columnIdentifier
 {
-    return 52.0f;
+	return floor((self.lineHeight-1) / 2) * 2 + 1;
 }
 
 - (NSImage*)imageForLine:(NSUInteger)lineNumber inColumnWithIdentifier:(id)columnIdentifier state:(GutterViewRowState)rowState
