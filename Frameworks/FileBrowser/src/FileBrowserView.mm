6108f747ecbcd6b0b4bd5da71f4ea9f954981617
iW123 <65850431+iW123@users.noreply.github.com>
2026-07-07 18:23:38 +0800

    Update FileBrowserView.mm

--- Frameworks/FileBrowser/src/FileBrowserView.mm
+++ Frameworks/FileBrowser/src/FileBrowserView.mm
@@ -71,7 +71,7 @@
 
 		_outlineView.backgroundColor = NSColor.clearColor;
         _scrollView.drawsBackground  = YES;
-        _scrollView.backgroundColor  = [NSColor colorWithRed:0.12 green:0.12 blue:0.12 alpha:0.3];
+        _scrollView.backgroundColor  = [NSColor colorWithRed:0.12 green:0.12 blue:0.12 alpha:0.1];
 	}
 	return self;
 }
